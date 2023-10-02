c0d2277a554bfb41019e32e3e768dadf195b5023
Keny09 <rosekendy09@gmail.com>
2023-10-02 05:04:40 -0700

    configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 #The code will create a file name schoolinside the /tmp directory
 file { '/tmp/school':
-content => 'I love Puppet',
+ content => 'I love Puppet',
  mode    => '0744',
  owner   => 'www-data',
  group   => 'www-data',
