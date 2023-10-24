cb8c4bbd926a34cfffcbac8fe2e0adaa4d3bad80
Keny09 <rosekendt09@gmail.com>
2023-10-23 19:10:40 -0700

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,45 @@
 #!/usr/bin/python3
-"""a module that request data and save in json format"""
-import csv
-import json
-import requests
-from sys import argv
+"""A script that uses api to store information
+in a csv file"""
 
+if __name__ == '__main__':
+    import requests
+    import sys
+    import json
 
-if __name__ == "__main__":
-    filename = "todo_all_employees.json"
-    url = "https://jsonplaceholder.typicode.com"
-    try:
-        res_user = requests.get(url + "/users/")
-        user = res_user.json()
-    except:
-        exit()
-    try:
-        res_todo = requests.get(url + "/todos")
-        things = res_todo.json()
-    except:
-        exit()
-    row = {}
-    with open(filename, "w") as f:
-        for u_id in user:
-            json_list = []
-            for thing in things:
-                row_dict = {}
-                if thing.get("userId") == u_id.get("id"):
-                    row_dict["username"] = u_id.get("username")
-                    row_dict["task"] = thing.get("title")
-                    row_dict["completed"] = thing.get("completed")
-                    json_list.append(row_dict)
-            row[u_id["id"]] = json_list
-        json.dumps(row)
-        f.write(str(row))
+    api_todo = 'https://jsonplaceholder.typicode.com/todos'
+    api_user = 'https://jsonplaceholder.typicode.com/users'
+
+    all_user = requests.get(api_user)
+    employee_details = all_user.json()
+
+    filename = 'todo_all_employees.json'
+    employee_dict = {}
+
+    for employee in employee_details:
+        id = employee.get('id')
+        id = int(id)
+
+        value_todo = {'userId': id}
+        value_user = {'id': id}
+
+        r_todo = requests.get(api_todo, params=value_todo)
+        r_user = requests.get(api_user, params=value_user)
+
+        todo_list = r_todo.json()
+        user_list = r_user.json()
+
+        username = user_list[0].get('username')
+        ar_dict = []
+
+        for data in todo_list:
+            new_dict = {}
+            new_dict['username'] = username
+            new_dict['task'] = data.get('title')
+            new_dict['completed'] = data.get('completed')
+            ar_dict.append(new_dict)
+
+        employee_dict[str(id)] = ar_dict
+
+    with open(filename, mode='w') as newfile:
+        json.dump(employee_dict, newfile)
