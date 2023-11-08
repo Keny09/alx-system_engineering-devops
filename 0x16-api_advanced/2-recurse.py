4a3f959338cd0ae831a55c6f86fb15eb5b457d52
Keny09 <rosekendy09@gmail.com>
2023-11-08 01:58:46 +0000

    advanced api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""a module recursively requests Reddit API to get data"""
+"""
+recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit
+"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=25):
-    """a function prints titles of all hot articles for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/.json'.format(subreddit)
-    header = {'User-Agent': 'Reddit API test'}
-    param = {'after': after}
-    response = requests.get(url, headers=header, allow_redirects=False,
-                            params=param)
-    dict = response.json()
-    if dict.get("error", 200) == 404:
+def recurse(subreddit, hot_list=[], count=0, next_page=None):
+    """return list containing titles of all hot articles"""
+    headers = {
+        "User-Agent": "0x16. API_advanced-e_kiminza"
+    }
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    params = {"limit": 50, "next_page": next_page, "count": count}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code != 200:
         return None
-    if after is None:
-        return hot_list
-    l_list = dict.get("data").get("children")
-    for dic in l_list:
-        hot_list.append(dic.get("data").get("title"))
-    pa = dict.get("data").get("after")
-    return recurse(subreddit, hot_list, pa)
+    response_ = response.json().get("data")
+    next_page = response_.get("next_page")
+    count += response_.get("dist")
+    children = response_.get("children")
+    for child in children:
+        title = child.get("data").get("title")
+        hot_list.append(title)
+    if next_page is not None:
+        return recurse(subreddit, hot_list, count, next_page)
+    return hot_list
